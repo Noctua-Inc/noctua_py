96e84eab609fef1bfdf2652b790de76353dee4de
JoaoPedroAlvesTec <joao.salves@sptech.school>
2026-09-10 16:25:23 -0300

    Feat:Facilitação de compreensão

--- Writer.py
+++ Writer.py
@@ -8,6 +8,7 @@ import socket
  
 EMAIL = "isabella.csantos@techsolutions.com.br"
 SENHA = "Bella2312@"
+HOSTNAMELOCAL = socket.gethostname
 URL_AUTENTICACAO = "http://127.0.0.1:3000/api/autenticacao"
 
 CAMINHO_CSV = "dadosBrutos.csv"
@@ -20,10 +21,11 @@ dados = []
 def autenticarComponentes(componentes):
     global flags_monitoramento
     
-    flags_monitoramento = {comp["tipo"]: True for comp in componentes}
 
+    for comp in componentes:
+        flags_monitoramento[comp["tipo"]] = True
+        
 
- 
 def leitura():
 
     arquivo_novo = (
@@ -35,17 +37,40 @@ def leitura():
         writer = csv.writer(csvfile)
  
         if arquivo_novo:
-            writer.writerow(['HostName', 'CPU', 'RAM', 'Disco', 'Swap', 'Load', 'Timestamp'])
- 
-        while True:
-            cpuAtual = psutil.cpu_percent(interval=1) if flags_monitoramento.get("CPU", False) else 0
-            ramAtual = psutil.virtual_memory().percent if flags_monitoramento.get("RAM", False) else 0
-            discoAtual = psutil.disk_usage("/").percent if flags_monitoramento.get("DISCO", False) else 0
-            swapAtual = psutil.swap_memory().percent if flags_monitoramento.get("SWAP", False) else 0
             
+            writer.writerow(['HostName', 'CPU', 'RAM', 'Disco', 'Swap', 'Load', 'Timestamp'])
+
+        while True:
+
+            if flags_monitoramento.get("CPU", False):
+                cpuAtual = psutil.cpu_percent(interval=1)
+            else:
+                cpuAtual = 0
+
+            # RAM
+            if flags_monitoramento.get("RAM", False):
+                ramAtual = psutil.virtual_memory().percent
+            else:
+                ramAtual = 0
+
+            # DISCO
+            if flags_monitoramento.get("DISCO", False):
+                discoAtual = psutil.disk_usage("/").percent
+            else:
+                discoAtual = 0
+
+            # SWAP
+            if flags_monitoramento.get("SWAP", False):
+                swapAtual = psutil.swap_memory().percent
+            else:
+                swapAtual = 0
+
             # Cálculo de LOAD
-            loadAtual = (psutil.getloadavg()[0] / psutil.cpu_count()) * 100 if flags_monitoramento.get("LOAD", False) else 0
- 
+            if flags_monitoramento.get("LOAD", False):
+                loadAtual = (psutil.getloadavg()[0] / psutil.cpu_count()) * 100
+            else:
+                loadAtual = 0
+    
             timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
  
             linha = [HOSTNAMELOCAL, cpuAtual, ramAtual, discoAtual, swapAtual, round(loadAtual, 2), timestamp]
